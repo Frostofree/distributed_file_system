420a7b287af00850ef40fb5b2cb73cea0978cc32
Amogha027 <aahalhalli@gmail.com>
2024-04-17 12:14:44 +0530

    master file

--- src/master.py
+++ src/master.py
@@ -7,9 +7,9 @@ import random
 
 
 class File():
-	def __init__(self, dfs_path: str, size):
+	def _init_(self, dfs_path: str):
 		self.dfs_path = dfs_path
-		self.size = size
+		self.size = None
 		self.chunks = {}
 		self.status = None
 		
@@ -27,15 +27,14 @@ class MessageHandler():
 
 
 class MasterServer():
-	def __init__(self, host, port):
+	def _init_(self, host, port):
 		self.host = host
 		self.port = port
 		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR)
 		self.sock.bind((self.host, self.port))
 		self.NUM_CHUNKS = config.NUM_CHUNKS
-		self.chunk_map = {}
-		self.files = {}
+		self.files = {} # maps file path to file object
 
 
 	def listen(self):
@@ -54,33 +53,26 @@ class MasterServer():
 		'''Check if file already exists, if not create a new file with the given number of chunks and return the chunk locations'''
 		if dfs_path in self.files:
 			return 'File already exists'
-		
-		
+		file = File(dfs_path) 
+		self.files[dfs_path] = file
+		return 'File created'
+	
 
-	def get_chunk_locs(self, num_chunks, dfs_path):
-		
-		chunk_locs = self.__get_chunk_locs(num_chunks)
-		self.files[dfs_path] = chunk_locs
-		return chunk_locs
-
-	def __get_chunk_locs(self, num_chunks):
-		# return a chunk id and a list of chunk server locatons based on replication factor
-		locs = []
-		for chunk in num_chunks:
-			chunk_id = self.__assign_chunk_id()
-			chunk_servers = self.__assign_chunk_server()
-			locs.append((chunk_id, chunk_servers))
-			self.chunk_map[chunk_id] = chunk_servers
-		return locs
+	def get_chunk_locs(self, num_chunks, dfs_path, current_chunk): 
+		# has to send back a uid and a list of chunk locations
+		chunk_handle = self.__assign_chunk_handle()
+		chunk_servers = self.__assign_chunk_server()
+		self.chunk_map[chunk_handle] = chunk_servers
+		file = self.files[dfs_path]
+		file.chunks[current_chunk] = (chunk_handle, chunk_servers)
+		return (chunk_handle, chunk_servers)
 	
 	def __assign_chunk_server(replication_factor = config.REPLICATION_FACTOR):
 		return random.choices(config.CHUNK_PORTS, replication_factor)
 
-	def __assign_chunk_id():
+	def __assign_chunk_handle():
 		return str(uuid.uuid4())
 	
-
-
 
 
 if __name__ == '__main__':
